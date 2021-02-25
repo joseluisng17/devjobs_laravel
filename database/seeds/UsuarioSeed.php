d08d3f85f6fb99a139a3fb0469ee703d4555b307
Jose Luis <joselunajarg@gmail.com>
2021-02-25 12:50:08 -0600

    first commit

---
+++ database/seeds/UsuarioSeed.php
@@ -0,0 +1,35 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsuarioSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Jose',
+            'email' => 'jose@gmail.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('12345678'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Luis',
+            'email' => 'luis@gmail.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('12345678'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
